24af9db33438548a68bef1c52f440bfbf23be3a2
danielmatt-dev <danielmatt1701@gmail.com>
2025-03-12 18:04:49 -0600

    Componente reporte [POR TERMINAR]

---
+++ src/app/presentation/reporte/reporte.component.ts
@@ -0,0 +1,46 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { Calendar } from 'primeng/calendar';
+import { Button, ButtonIcon } from 'primeng/button';
+import { Table, TableModule } from 'primeng/table';
+import { InputText } from 'primeng/inputtext';
+
+@Component({
+    selector: 'app-reporte',
+    standalone: true,
+    imports: [DropdownModule, FormsModule, Calendar, Button, TableModule, InputText, ButtonIcon],
+    templateUrl: './reporte.component.html',
+    styleUrl: './reporte.component.scss'
+})
+export class ReporteComponent implements AfterViewInit {
+    @ViewChild(Calendar) calendar!: Calendar;
+
+    afecciones = ['Opción 1', 'Opción 2', 'Opción 3'];
+    afeccionSeleccionada?: string = undefined;
+    resultados = ['Opción 1', 'Opción 2', 'Opción 3'];
+    resultadoSeleccionado?: string = undefined;
+
+    fechasSeleccionadas: Date[] = [];
+
+    ngAfterViewInit(): void {}
+
+    async onRowSelect(event: any) {}
+
+    clear(table: Table) {
+        table.clear();
+    }
+
+    onGlobalFilter(table: Table, event: Event) {
+        table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+        console.log('Key Down:', event.key);
+    }
+
+    onKeyUp(event: KeyboardEvent) {
+        console.log('Key Up:', event.key);
+    }
+}
+
