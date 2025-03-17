e35b5c6087dfc69822c3be13671618da29378080
Daniel <danielmatt1701@gmail.com>
2025-03-17 12:05:35 -0600

    feat(Reportes): Añadir gráficos de reportes
    - Se agregaron los gráficos de línea, barras y pastel al componente de reportes.
    - Se implementó CSS Grid y Flexbox para garantizar la responsividad en diferentes tamaños de pantalla.
    - Se ajustó la altura de los gráficos para evitar espacios en blanco innecesarios en pantallas grandes.
    - Se aseguraron tamaños adecuados en móviles y tablets para mejorar la visualización.

--- src/app/presentation/reporte/reporte.component.ts
+++ src/app/presentation/reporte/reporte.component.ts
@@ -6,15 +6,19 @@ import { ButtonModule } from 'primeng/button';
 import { Table, TableModule } from 'primeng/table';
 import { InputText } from 'primeng/inputtext';
 import { Select } from 'primeng/select';
+import { Fluid } from 'primeng/fluid';
+import { UIChart } from 'primeng/chart';
+import { LayoutService } from '../../layout/service/layout.service';
+import { debounceTime, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-reporte',
     standalone: true,
-    imports: [DropdownModule, FormsModule, Calendar, TableModule, InputText, ButtonModule, Select],
+    imports: [DropdownModule, FormsModule, Calendar, TableModule, InputText, ButtonModule, Select, Fluid, UIChart],
     templateUrl: './reporte.component.html',
     styleUrl: './reporte.component.scss'
 })
-export class ReporteComponent implements AfterViewInit {
+export class ReporteComponent {
     @ViewChild(Calendar) calendar!: Calendar;
 
     afecciones = ['Opción 1', 'Opción 2', 'Opción 3'];
@@ -24,7 +28,165 @@ export class ReporteComponent implements AfterViewInit {
 
     fechasSeleccionadas: Date[] = [];
 
-    ngAfterViewInit(): void {}
+    lineData: any;
+
+    barData: any;
+
+    pieData: any;
+
+    lineOptions: any;
+
+    barOptions: any;
+
+    pieOptions: any;
+
+    subscription: Subscription;
+
+    constructor(private readonly layoutService: LayoutService) {
+        this.subscription = this.layoutService.configUpdate$.pipe(debounceTime(25)).subscribe(() => {
+            this.initCharts();
+        });
+    }
+
+    initCharts() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+        this.barData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-200'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-200'),
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+        this.barOptions = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                        font: {
+                            weight: 500
+                        }
+                    },
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
+
+        this.pieData = {
+            labels: ['A', 'B', 'C'],
+            datasets: [
+                {
+                    data: [540, 325, 702],
+                    backgroundColor: [documentStyle.getPropertyValue('--p-indigo-500'), documentStyle.getPropertyValue('--p-purple-500'), documentStyle.getPropertyValue('--p-teal-500')],
+                    hoverBackgroundColor: [documentStyle.getPropertyValue('--p-indigo-400'), documentStyle.getPropertyValue('--p-purple-400'), documentStyle.getPropertyValue('--p-teal-400')]
+                }
+            ]
+        };
+
+        this.pieOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    position: 'top',
+                    labels: {
+                        usePointStyle: true,
+                        color: textColor
+                    }
+                }
+            }
+        };
+
+        this.lineData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    tension: 0.4
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-200'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-200'),
+                    tension: 0.4
+                }
+            ]
+        };
+
+        this.lineOptions = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
+
+    }
 
     async onRowSelect(event: any) {}
 
