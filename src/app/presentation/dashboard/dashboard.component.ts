94ee1250fe8ea3a8fd84a08e3d4e37ac8fcc91c9
Daniel <danielmatt1701@gmail.com>
2025-03-18 23:55:52 -0600

    feature(dashboard): Gŕaficas de barra
    - Se agregaron **gráficas de barras** para visualizar las detecciones:
      - **Detecciones por mes**.
      - **Detecciones por rango de edad y afección**.
    - Se implementó una **estructura responsiva** con `grid grid-cols-1 md:grid-cols-2` para mostrar **una gráfica por fila en móviles** y **dos en pantallas medianas en adelante**.
    - Se ajustó el **tamaño y peso de los títulos** en las tarjetas, cambiando `font-semibold text-xl` a `font-medium text-xl`.

--- src/app/presentation/dashboard/dashboard.component.ts
+++ src/app/presentation/dashboard/dashboard.component.ts
@@ -1,11 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { debounceTime, Subscription } from 'rxjs';
+import { LayoutService } from '../../layout/service/layout.service';
+import { Fluid } from 'primeng/fluid';
+import { UIChart } from 'primeng/chart';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [Fluid, UIChart],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+    barData1: any;
+    barOptions1: any;
+    barData2: any;
+    barOptions2: any;
 
+    subscription: Subscription;
+
+    constructor(private readonly layoutService: LayoutService) {
+        this.subscription = this.layoutService.configUpdate$.pipe(debounceTime(25)).subscribe(() => {
+            this.initCharts();
+        });
+    }
+
+    ngOnInit(): void {
+        this.initCharts();
+    }
+
+    initCharts() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+        this.barData1 = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+            ]
+        };
+
+        this.barOptions1 = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                        font: {
+                            weight: 500
+                        }
+                    },
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
+
+        this.barData2 = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-500'),
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--p-primary-200'),
+                    borderColor: documentStyle.getPropertyValue('--p-primary-200'),
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+        this.barOptions2 = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                        font: {
+                            weight: 500
+                        }
+                    },
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
+    }
 }
