8058ff8388cae00d7a0ae8c2902d30c8a131d6a2
Daniel <danielmatt1701@gmail.com>
2025-03-31 00:44:24 -0600

    feat(ver-detalle): Agregar vista del paciente
    - Primera card con datos del paciente
    - Segunda card con los datos adicionales a la inspección del paciente
    - Tercera card que muestra el registro de inspecciones mediante una tabla

--- src/app/presentation/ver-detalle-inspeccion/ver-detalle-inspeccion.component.ts
+++ src/app/presentation/ver-detalle-inspeccion/ver-detalle-inspeccion.component.ts
@@ -3,19 +3,21 @@ import { InputText } from 'primeng/inputtext';
 import { Button } from 'primeng/button';
 import { ProgressBar } from 'primeng/progressbar';
 import { PrimeTemplate } from 'primeng/api';
+import { TableModule } from 'primeng/table';
 
 @Component({
     selector: 'app-ver-detalle-inspeccion',
     standalone: true,
-    imports: [InputText, Button, ProgressBar, PrimeTemplate],
+    imports: [InputText, Button, ProgressBar, PrimeTemplate, TableModule],
     templateUrl: './ver-detalle-inspeccion.component.html',
     styleUrl: './ver-detalle-inspeccion.component.scss'
 })
 export class VerDetalleInspeccionComponent {
-
     red = '#f87171';
     amber = '#FB923C';
     blue = '#38BDF8';
     green = '#4ADE80';
+
+    async onRowSelect(event: any) {}
 
 }
