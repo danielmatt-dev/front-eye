64758c2568b6f8d2df12893f13c244763c353ed4
Daniel <danielmatt1701@gmail.com>
2025-03-31 21:54:38 -0600

    feat(datos-geograficos): Agregar plugin de filtros
    - Se añadió el plugin Leaflet.Control.Layers.Tree para gestionar los filtros en el mapa.
    - Se configuraron las opciones de filtrado según las opciones definidas.
    - Se creó el archivo de estilos "src/assets/leaflet/control-layers-tree/L.Control.Layers.Tree.css" para definir en el futuro los estilos de filtrado.

--- src/app/presentation/datos-geograficos/datos-geograficos.component.ts
+++ src/app/presentation/datos-geograficos/datos-geograficos.component.ts
@@ -6,6 +6,7 @@ import { NgClass, NgForOf } from '@angular/common';
 import * as L from 'leaflet';
 import 'leaflet.featuregroup.subgroup';
 import 'leaflet.markercluster';
+import 'leaflet.control.layers.tree'
 
 @Component({
     selector: 'app-datos-geograficos',
@@ -16,7 +17,7 @@ import 'leaflet.markercluster';
 })
 export class DatosGeograficosComponent implements AfterViewInit {
 
-    private leafletMap!: L.Map;
+    private readonly leafletMap!: L.Map;
 
     fechasSeleccionadas: Date[] = [];
     calendarDisabled = true;
@@ -29,12 +30,13 @@ export class DatosGeograficosComponent implements AfterViewInit {
     ];
 
     ngAfterViewInit() {
-        this.initMap()
+        this.initMap();
     }
 
     private initMap() {
         const map = L.map('map').setView([51.505, -0.09], 13);
 
+        // Capa base de OpenStreetMap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '© OpenStreetMap'
@@ -43,7 +45,7 @@ export class DatosGeograficosComponent implements AfterViewInit {
         const markerClusterGroup = L.markerClusterGroup({
             spiderfyOnMaxZoom: true,
             showCoverageOnHover: false,
-            zoomToBoundsOnClick: false,
+            zoomToBoundsOnClick: false
         });
 
         const dataPoints = [
@@ -52,10 +54,10 @@ export class DatosGeograficosComponent implements AfterViewInit {
             { lat: 51.506, lng: -0.09, name: 'C' },
             { lat: 51.507, lng: -0.085, name: 'D' },
             { lat: 51.508, lng: -0.086, name: 'E' },
-            { lat: 51.51,  lng: -0.1,   name: 'F' },
+            { lat: 51.51, lng: -0.1, name: 'F' }
         ];
 
-        dataPoints.forEach(p => {
+        dataPoints.forEach((p) => {
             const m = L.marker([p.lat, p.lng]).bindPopup(`<b>${p.name}</b>`);
             markerClusterGroup.addLayer(m);
         });
@@ -63,12 +65,51 @@ export class DatosGeograficosComponent implements AfterViewInit {
         map.addLayer(markerClusterGroup);
 
         // Cuando haces click en un cluster → spiderfy (no bug)
-        markerClusterGroup.on('clusterclick', e => e.propagatedFrom.spiderfy());
+        markerClusterGroup.on('clusterclick', (e) => e.propagatedFrom.spiderfy());
 
         // Asegura recalcular tamaño al cambiar contenedor
         new ResizeObserver(() => map.invalidateSize()).observe(document.getElementById('map')!);
-    }
 
+        const filtros = {
+                label: 'Filtros',
+                children: [
+                    {
+                        label: 'Afección',
+                        children: [
+                            { label: 'DMAE Seca', layer: L.layerGroup() },
+                            { label: 'DMAE Húmeda', layer: L.layerGroup() },
+                            { label: 'Retinopatía Diabética', layer: L.layerGroup() }
+                        ],
+                    },
+                    {
+                        label: 'Rango de Edad',
+                        children: [
+                            { label: 'Menos de 30', layer: L.layerGroup() },
+                            { label: 'De 30 a 45', layer: L.layerGroup() },
+                            { label: 'Mas de 45', layer: L.layerGroup() }
+                        ]
+                    },
+                    {
+                        label: 'Género',
+                        children: [
+                            { label: 'Hombre', layer: L.layerGroup() },
+                            { label: 'Mujer', layer: L.layerGroup() },
+                        ],
+                    },
+                    {
+                        label: 'Clasificación de afección',
+                        children: [
+                            { label: 'Leve', layer: L.layerGroup() },
+                            { label: 'Moderada', layer: L.layerGroup() },
+                            { label: 'Proliferativa', layer: L.layerGroup() }
+                        ]
+                    }
+                ]
+            };
+
+        const treeControl = (L.control as any).layers.tree(null, filtros, { collapsed: true });
+        treeControl.addTo(map);
+    }
 
     @HostListener('window:resize')
     onWindowResize(): void {
