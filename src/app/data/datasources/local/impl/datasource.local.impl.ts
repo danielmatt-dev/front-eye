bdb2fd92eaa96e7ae5126fd13e3216d7c3814762
Daniel <danielmatt1701@gmail.com>
2025-05-05 15:14:39 -0600

    Agregar capa de data
    - Se añadió una capa donde de define el acceso a datos
    - Se creó un datasource local para guardar y recuperar datos locales
    - Se definió una interface con los métodos para recuperar y guardar el
      rol y token del usuario

---
+++ src/app/data/datasources/local/impl/datasource.local.impl.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+class DatasourceLocalImpl implements DatasourceLocal {
+
+    setToken(token: string) {
+        localStorage.setItem('token', token);
+    }
+
+    getToken(): string | null {
+        return localStorage.getItem('token');
+    }
+
+    setRole(role: string) {
+        localStorage.setItem('role', role);
+    }
+
+    getRole(): string | null {
+        return localStorage.getItem('role')
+    }
+
+}
