3268c2e2317349a9c0821ff2d7f132340d54097c
Daniel <danielmatt1701@gmail.com>
2025-05-05 23:12:43 -0600

    Generar componente para Login
    - Se generó un nuevo componente para el inicio de sesión
    - Se cambió el antiguo componente de Login de la plantilla por
      LoginComponent
    - Se añadió una lógica para redirigir a los usuarios dependiendo de un
      rol que ingresen en email

---
+++ src/app/presentation/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { RippleModule } from 'primeng/ripple';
+import { AppFloatingConfigurator } from '../../layout/component/app.floatingconfigurator';
+import { DatasourceLocalImpl } from '../../data/datasources/local/impl/datasource.local.impl';
+
+@Component({
+    selector: 'app-login',
+    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator],
+    templateUrl: './login.component.html',
+    standalone: true,
+    styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+    email: string = '';
+
+    password: string = '';
+
+    checked: boolean = false;
+
+    constructor(
+        private readonly local: DatasourceLocalImpl,
+        private readonly router: Router
+    ) {}
+
+    async redirigir() {
+
+        //const userRole = this.local.getRole()
+        let userRole = 'ADMIN'
+
+        this.email = this.email.toLowerCase()
+        if (this.email.includes('doctor')) {
+            this.local.setRole('DOCTOR')
+        } else {
+            this.local.setRole('ADMIN')
+        }
+
+        if (userRole === 'ADMIN') {
+            await this.router.navigate(['/insights/dashboard'])
+        }
+
+        if (userRole === 'DOCTOR') {
+            await this.router.navigate(['/insights/nueva-inspeccion'])
+        }
+
+    }
+
+}
