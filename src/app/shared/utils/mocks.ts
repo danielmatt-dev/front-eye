b5f09f2e2ec55cf9c692ceb452bd98ea233d13b6
Daniel <danielmatt1701@gmail.com>
2025-05-10 23:58:36 -0600

    Actualizar ReporteComponent
    - Se añadieron funcionalidades y validaciones para el filtrado de datos
      de la tabla
    - Se actualizaron los mocks para la tabla
    - Se aplicó el ordenamiento de las columnas en la tabla

--- src/app/shared/utils/mocks.ts
+++ src/app/shared/utils/mocks.ts
@@ -10,27 +10,27 @@ export const afecciones = [
 ]
 
 export const reports = [
-    { id: 'c496f7', fecha: '21/05/2024', hora: '06:14', edad: 33, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Proliferativo' },
-    { id: '4cd866', fecha: '04/05/2024', hora: '16:38', edad: 66, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Proliferativo' },
-    { id: '2f2770', fecha: '01/05/2024', hora: '00:35', edad: 28, afeccion: 'Ret. Diabetica', ojo: 'Derecho', resultado: 'Moderado' },
-    { id: '44dfc1', fecha: '24/05/2024', hora: '06:45', edad: 88, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Leve' },
-    { id: 'aacfcb', fecha: '20/05/2024', hora: '20:44', edad: 55, afeccion: 'Ret. Diabetica', ojo: 'Derecho', resultado: 'Leve' },
-    { id: 'd01787', fecha: '08/05/2024', hora: '17:26', edad: 64, afeccion: 'Ret. Diabetica', ojo: 'Izquierdo', resultado: 'Proliferativo' },
-    { id: '8da49a', fecha: '08/05/2024', hora: '07:28', edad: 42, afeccion: 'Ret. Diabetica', ojo: 'Izquierdo', resultado: 'Sin Afeccion' },
-    { id: '5484b0', fecha: '05/05/2024', hora: '18:17', edad: 26, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Sin Afeccion' },
-    { id: '3707a9', fecha: '24/05/2024', hora: '00:48', edad: 23, afeccion: 'Ret. Diabetica', ojo: 'Derecho', resultado: 'Sin Afeccion' },
-    { id: '7c2861', fecha: '20/05/2024', hora: '05:44', edad: 47, afeccion: 'Ret. Diabetica', ojo: 'Derecho', resultado: 'Leve' },
-    { id: 'ec3030', fecha: '22/05/2024', hora: '13:21', edad: 55, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Leve' },
-    { id: '5c9b55', fecha: '24/05/2024', hora: '08:09', edad: 28, afeccion: 'Ret. Diabetica', ojo: 'Derecho', resultado: 'Proliferativo' },
-    { id: 'c7378f', fecha: '29/05/2024', hora: '06:48', edad: 47, afeccion: 'Ret. Diabetica', ojo: 'Izquierdo', resultado: 'Sin Afeccion' },
-    { id: 'bf81bd', fecha: '12/05/2024', hora: '10:06', edad: 30, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Proliferativo' },
-    { id: '1b32a4', fecha: '03/05/2024', hora: '02:24', edad: 66, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Proliferativo' },
-    { id: '8f03f9', fecha: '19/05/2024', hora: '03:22', edad: 53, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Proliferativo' },
-    { id: '006644', fecha: '14/05/2024', hora: '11:38', edad: 76, afeccion: 'DMAE Humeda', ojo: 'Izquierdo', resultado: 'Moderado' },
-    { id: '3d99fe', fecha: '02/05/2024', hora: '08:51', edad: 64, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Sin Afeccion' },
-    { id: 'c14b33', fecha: '01/05/2024', hora: '01:46', edad: 38, afeccion: 'Ret. Diabetica', ojo: 'Derecho', resultado: 'Sin Afeccion' },
-    { id: 'a11288', fecha: '03/05/2024', hora: '14:34', edad: 65, afeccion: 'Ret. Diabetica', ojo: 'Izquierdo', resultado: 'Moderado' }
-]
+    { id: 'c496f7', fecha: '21/05/2025', hora: '06:14 AM', edad: 33, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Proliferativo' },
+    { id: '4cd866', fecha: '04/05/2025', hora: '04:38 PM', edad: 66, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Proliferativo' },
+    { id: '2f2770', fecha: '01/05/2025', hora: '12:35 AM', edad: 28, afeccion: 'Retinopatía Diabética', ojo: 'Derecho', resultado: 'Moderado' },
+    { id: '44dfc1', fecha: '24/05/2025', hora: '06:45 AM', edad: 88, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Leve' },
+    { id: 'aacfcb', fecha: '20/05/2025', hora: '08:44 PM', edad: 55, afeccion: 'Retinopatía Diabética', ojo: 'Derecho', resultado: 'Leve' },
+    { id: 'd01787', fecha: '08/05/2025', hora: '05:26 PM', edad: 64, afeccion: 'Retinopatía Diabética', ojo: 'Izquierdo', resultado: 'Proliferativo' },
+    { id: '8da49a', fecha: '08/05/2025', hora: '07:28 AM', edad: 42, afeccion: 'Retinopatía Diabética', ojo: 'Izquierdo', resultado: 'Sin Afeccion' },
+    { id: '5484b0', fecha: '05/05/2025', hora: '06:17 PM', edad: 26, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Sin Afeccion' },
+    { id: '3707a9', fecha: '24/05/2025', hora: '12:48 AM', edad: 23, afeccion: 'Retinopatía Diabética', ojo: 'Derecho', resultado: 'Sin Afeccion' },
+    { id: '7c2861', fecha: '20/05/2025', hora: '05:44 AM', edad: 47, afeccion: 'Retinopatía Diabética', ojo: 'Derecho', resultado: 'Leve' },
+    { id: 'ec3030', fecha: '22/05/2025', hora: '01:21 PM', edad: 55, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Leve' },
+    { id: '5c9b55', fecha: '24/05/2025', hora: '08:09 AM', edad: 28, afeccion: 'Retinopatía Diabética', ojo: 'Derecho', resultado: 'Proliferativo' },
+    { id: 'c7378f', fecha: '29/05/2025', hora: '06:48 AM', edad: 47, afeccion: 'Retinopatía Diabética', ojo: 'Izquierdo', resultado: 'Sin Afeccion' },
+    { id: 'bf81bd', fecha: '12/05/2025', hora: '10:06 AM', edad: 30, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Proliferativo' },
+    { id: '1b32a4', fecha: '03/05/2025', hora: '02:24 AM', edad: 66, afeccion: 'DMAE Seca', ojo: 'Izquierdo', resultado: 'Proliferativo' },
+    { id: '8f03f9', fecha: '19/05/2025', hora: '03:22 AM', edad: 53, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Proliferativo' },
+    { id: '006644', fecha: '14/05/2025', hora: '11:38 AM', edad: 76, afeccion: 'DMAE Humeda', ojo: 'Izquierdo', resultado: 'Moderado' },
+    { id: '3d99fe', fecha: '02/05/2025', hora: '08:51 AM', edad: 64, afeccion: 'DMAE Humeda', ojo: 'Derecho', resultado: 'Sin Afeccion' },
+    { id: 'c14b33', fecha: '01/05/2025', hora: '01:46 AM', edad: 38, afeccion: 'Retinopatía Diabética', ojo: 'Derecho', resultado: 'Sin Afeccion' },
+    { id: 'a11288', fecha: '03/05/2025', hora: '02:34 PM', edad: 65, afeccion: 'Retinopatía Diabética', ojo: 'Izquierdo', resultado: 'Moderado' }
+];
 
 export const dataPointsMocks = [
     // Centros de interés y turísticos
@@ -231,7 +231,7 @@ export const inspecciones = [
         fecha: '2023-08-15',
         hora: '11:45',
         edad: 58,
-        afeccion: 'Ret. Diabética',
+        afeccion: 'Retinopatía Diabética',
         ojo: 'Derecho',
         resultado: 'Leve'
     },
@@ -249,7 +249,7 @@ export const inspecciones = [
         fecha: '2023-08-16',
         hora: '12:00',
         edad: 29,
-        afeccion: 'Ret. Diabética',
+        afeccion: 'Retinopatía Diabética',
         ojo: 'Derecho',
         resultado: 'Proliferativo'
     },
