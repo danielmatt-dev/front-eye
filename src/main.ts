a0af6a99b54e34e20210142be06da6d225e7edbd
Daniel <danielmatt1701@gmail.com>
2025-06-09 17:38:01 -0600

    Implementar variables de entorno
    - Se añadió un archivo para definir las variables de entorno de la api
    - Se importó reflect-metadata para la serialización y deserialización

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
+import 'reflect-metadata';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
