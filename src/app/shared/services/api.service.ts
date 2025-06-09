aaf10d770ebf051b3a7539204528c4c39eadd7c9
Daniel <danielmatt1701@gmail.com>
2025-06-09 15:59:31 -0600

    Implementar ApiService
    - Se implementó un service para enviar peticiones con HTTP

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+    BadCredencialsException,
+    BadRequestException,
+    ForbiddenException, InternalServerException,
+    ResourceNotFoundException, TimeoutException
+} from '../exceptions/exceptions';
+import { catchError, firstValueFrom, Observable, of } from 'rxjs';
+import { Either, left, right } from 'fp-ts/Either';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+
+    constructor(private readonly http: HttpClient) {}
+
+    protected sendRequest<T>(obs$: Observable<T>): Promise<Either<Error, T>> {
+        return firstValueFrom(
+            obs$.pipe(
+                map(data => right(data)),
+                catchError((err: HttpErrorResponse) =>
+                    of(left(this.mapException(err))))
+            )
+        )
+    }
+
+    private mapException(error: HttpErrorResponse): Error {
+        switch (error.status) {
+            case 400: return new BadRequestException(error.message);
+            case 401: return new BadCredencialsException(error.message);
+            case 403: return new ForbiddenException(error.message);
+            case 404: return new ResourceNotFoundException(error.message);
+            case 408: return new TimeoutException(error.message);
+            case 500: return new InternalServerException(error.message);
+            default:  return new Error(error.message);
+        }
+    }
+
+}
+
