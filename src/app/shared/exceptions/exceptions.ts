da6ddff293b7ccfe3d520ccdc15f6e6764dc95fa
Daniel <danielmatt1701@gmail.com>
2025-06-09 15:59:24 -0600

    Añadir excepciones personalizadas

---
+++ src/app/shared/exceptions/exceptions.ts
@@ -0,0 +1,11 @@
+export class BadCredencialsException extends Error {}
+
+export class ResourceNotFoundException extends Error { }
+
+export class BadRequestException extends Error { }
+
+export class InternalServerException extends Error {}
+
+export class TimeoutException extends Error {}
+
+export class ForbiddenException extends Error {}
