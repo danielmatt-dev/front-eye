6b27421a1f0a38ad9bbc16b4c553e5a08c5d9e97
Daniel <danielmatt1701@gmail.com>
2025-06-09 20:16:50 -0600

    Actualizar DoctorDatasourceRemote
    - Añadir funciones para guardar, actualizar y borrar doctor con llamadas
      http a la api

--- src/app/feature/doctor/data/datasource/remote/doctor.datasource.remote.ts
+++ src/app/feature/doctor/data/datasource/remote/doctor.datasource.remote.ts
@@ -1,9 +1,16 @@
 import { Either } from 'fp-ts/Either';
 import { DoctorResponseModel } from '../../models/doctor.response.model';
+import { DoctorRequestModel } from '../../models/doctor.request.model';
 
 // <>
 export interface DoctorDatasourceRemote {
 
+    postDoctor(request: DoctorRequestModel): Promise<Either<Error, DoctorResponseModel>>
+
+    putDoctor(doctorId: number, request: DoctorRequestModel): Promise<Either<Error, DoctorResponseModel>>
+
     getAllDoctors(): Promise<Either<Error, DoctorResponseModel[]>>
+
+    deleteDoctor(doctorId: number): Promise<Either<Error, boolean>>
 
 }
--- src/app/feature/doctor/data/datasource/remote/impl/doctor.datasource.remote.impl.ts
+++ src/app/feature/doctor/data/datasource/remote/impl/doctor.datasource.remote.impl.ts
@@ -5,16 +5,39 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../../../../../../shared/services/api.service';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { plainToInstance } from 'class-transformer';
+import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { DoctorEndpoints } from '../doctor.endpoints';
+import { DoctorRequestModel } from '../../../models/doctor.request.model';
 
+// <>
 @Injectable({ providedIn: 'root' })
 export class DoctorDatasourceRemoteImpl implements DoctorDatasourceRemote {
-
     constructor(
         private readonly http: HttpClient,
         private readonly apiService: ApiService
     ) {}
+
+    postDoctor(request: DoctorRequestModel): Promise<Either<Error, DoctorResponseModel>> {
+        const url = DoctorEndpoints.PATH
+        const obs$ = this.http
+            .post<DoctorResponseModel>(url, instanceToPlain(request))
+            .pipe(
+                map(response =>
+                    plainToInstance(DoctorResponseModel, response)))
+
+        return this.apiService.sendRequest<DoctorResponseModel>(obs$)
+    }
+
+    putDoctor(doctorId: number, request: DoctorRequestModel): Promise<Either<Error, DoctorResponseModel>> {
+        const url = `${DoctorEndpoints.PATH}/${doctorId}`
+        const obs$ = this.http
+            .put<DoctorResponseModel>(url, instanceToPlain(request))
+            .pipe(
+                map(response =>
+                    plainToInstance(DoctorResponseModel, response)))
+
+        return this.apiService.sendRequest<DoctorResponseModel>(obs$)
+    }
 
     getAllDoctors(): Promise<Either<Error, DoctorResponseModel[]>> {
         const url = DoctorEndpoints.PATH
@@ -27,6 +50,16 @@ export class DoctorDatasourceRemoteImpl implements DoctorDatasourceRemote {
             )
 
         return this.apiService.sendRequest<DoctorResponseModel[]>(obs$)
+    }
+
+    deleteDoctor(doctorId: number): Promise<Either<Error, boolean>> {
+        const url = `${DoctorEndpoints.PATH}/${doctorId}`
+        const obs$ = this.http
+            .delete(url)
+            .pipe(
+                map(() => true))
+
+        return this.apiService.sendRequest<boolean>(obs$)
     }
 
 }
