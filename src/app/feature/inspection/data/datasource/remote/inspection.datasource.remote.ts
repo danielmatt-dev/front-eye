657c2b6434d42f314fb9fe565def32309f225625
Daniel <danielmatt1701@gmail.com>
2025-06-11 21:28:04 -0600

    Actualizar InspectionDatasourceRemote
    - Se añadió una función para crear una nueva inspección
    - Se añadió una función para buscar el detalle de una inspeción por id

--- src/app/feature/inspection/data/datasource/remote/inspection.datasource.remote.ts
+++ src/app/feature/inspection/data/datasource/remote/inspection.datasource.remote.ts
@@ -1,8 +1,14 @@
 import { Either } from 'fp-ts/Either';
 import { InspectionReponseModel } from '../../models/inspection.reponse.model';
+import { InspectionRequestModel } from '../../models/inspection.request.model';
+import { InspectionDetailsModel } from '../../models/inspection.details.model';
 
 export interface InspectionDatasourceRemote {
 
+    postInspection(request: InspectionRequestModel): Promise<Either<Error, boolean>>
+
     getAllInspections(): Promise<Either<Error, InspectionReponseModel[]>>
+
+    getInspectionByInspectionId(inspectionId: number): Promise<Either<Error, InspectionDetailsModel>>
 
 }
