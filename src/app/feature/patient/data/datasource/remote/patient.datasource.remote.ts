4655f3d7c543a8b729f7539cf281758061f200c6
Daniel <danielmatt1701@gmail.com>
2025-06-11 23:27:24 -0600

    Actualizar PatientDatasourceRemote
    - Se añadieron las operaciones CRUD para Patient

--- src/app/feature/patient/data/datasource/remote/patient.datasource.remote.ts
+++ src/app/feature/patient/data/datasource/remote/patient.datasource.remote.ts
@@ -1,8 +1,20 @@
 import { Either } from 'fp-ts/Either';
 import { PatientWithInspectionsModel } from '../../models/patient.with.inspections.model';
+import { PatientRequestModel } from '../../models/patient.request.model';
+import { PatientResponseModel } from '../../models/patient.response.model';
 
 // <>
 export interface PatientDatasourceRemote {
+
+    postPatient(request: PatientRequestModel): Promise<Either<Error, PatientResponseModel>>
+
+    putPatient(request: PatientRequestModel, patientId: number): Promise<Either<Error, PatientResponseModel>>
+
+    getPatientById(patientId: number): Promise<Either<Error, PatientResponseModel>>
+
+    getAllPatients(): Promise<Either<Error, PatientResponseModel[]>>
+
+    deletePatients(patientIds: number[]): Promise<Either<Error, boolean>>
 
     getAllPatientsWithInspections(): Promise<Either<Error, PatientWithInspectionsModel[]>>
 
