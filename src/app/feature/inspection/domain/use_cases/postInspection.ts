870b93548745e3100c071ea69c6ae7fee2788ad6
Daniel <danielmatt1701@gmail.com>
2025-06-11 21:33:58 -0600

    Implementar caso de uso PostInspection

---
+++ src/app/feature/inspection/domain/use_cases/postInspection.ts
@@ -0,0 +1,16 @@
+import { Either } from 'fp-ts/lib/Either';
+import { UseCase } from '../../../../shared/utils/usecase';
+import { InspectionRequestEntity } from '../entity/inspection.request.entity';
+import { InspectionsDatasourceRemoteImpl } from '../../data/datasource/remote/impl/inspections.datasource.remote.impl';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class PostInspection implements UseCase<boolean, InspectionRequestEntity> {
+
+    constructor(private readonly remote: InspectionsDatasourceRemoteImpl) {}
+
+    call(params: InspectionRequestEntity): Promise<Either<Error, boolean>> {
+        return this.remote.postInspection(params)
+    }
+
+}
