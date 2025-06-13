4c7f14c96360675627ac2a45091cbd312be09133
Daniel <danielmatt1701@gmail.com>
2025-06-13 00:07:54 -0600

    Añadir función para toast success
    - Se añadió una función para enviar notificaciones toast dependiendo del
      caso de uso que resulta exitoso

--- src/app/feature/doctor/presentation/doctor-component/validation/doctor.component.helper.ts
+++ src/app/feature/doctor/presentation/doctor-component/validation/doctor.component.helper.ts
@@ -20,4 +20,10 @@ export class DoctorComponentHelper extends ValidatorHelper {
         return DoctorComponentHelper.instance
     }
 
+    sendToastMessageSuccess(type: 'createDoctor' | 'updateDoctor' | 'deleteDoctor' | 'deleteDoctors', param: string) {
+        const title = this.getText(`toast.success.titles.${type}`)
+        const message = this.getText(`toast.success.messages.${type}`).replace('@', param)
+        this.sendToastMessage({title: title, message: message, type: 'success'})
+    }
+
 }
