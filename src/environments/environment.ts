d807ed40fbc5b534d3a862494430a66872c2a536
Daniel <danielmatt1701@gmail.com>
2025-06-13 21:41:51 -0600

    Actualizar caso de uso UpdatePatient

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiBaseUrl: 'http://localhost:8080/insights',
-    token: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU4iLCJ1c2VySWQiOjEwLCJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE3NDkyODIxNjMsImV4cCI6MTc2NzQyNjE2M30.62smU3pf28vN1GSXd2cad4JndOjict4abF5AIu5t4u4'
+    token: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiRE9DVE9SIiwidXNlcklkIjoxOSwic3ViIjoiZGFuaWVsbWFydGluZXpnOTA3OEBnbWFpbC5jb20iLCJpYXQiOjE3NDk4NzA3MTYsImV4cCI6MTc2ODAxNDcxNn0.WHAQFshPfiiQ174J1jPmGN9BmfkPPxG3iq1wXpXXiAg'
 };
