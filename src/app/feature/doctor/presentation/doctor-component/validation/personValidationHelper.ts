abdc490825e34715ef16ba3fb4f0d0dec4767765
Daniel <danielmatt1701@gmail.com>
2025-06-13 21:35:58 -0600

    Actualizar ValidatorHelper
    - Se actualizó el helper para validar los campos de Doctor y
      Patient

--- src/app/feature/doctor/presentation/doctor-component/validation/personValidationHelper.ts
+++ src/app/feature/doctor/presentation/doctor-component/validation/personValidationHelper.ts
@@ -3,22 +3,23 @@ import { MessageService } from 'primeng/api';
 import { TranslateService } from '@ngx-translate/core';
 import { PrimeNG } from 'primeng/config';
 import { ClinicEntity } from '../../../../clinic/domain/entity/clinic.entity';
+import validator from 'validator';
 
-export class DoctorComponentHelper extends ValidatorHelper {
+export class PersonValidationHelper extends ValidatorHelper {
 
-    private static instance: DoctorComponentHelper
+    private static instance: PersonValidationHelper
 
     static getInstance(
         messageService: MessageService,
         translateService: TranslateService,
         primeng: PrimeNG
-    ): DoctorComponentHelper {
+    ): PersonValidationHelper {
 
-        if (!DoctorComponentHelper.instance) {
-            DoctorComponentHelper.instance = new DoctorComponentHelper(messageService, translateService, primeng)
+        if (!PersonValidationHelper.instance) {
+            PersonValidationHelper.instance = new PersonValidationHelper(messageService, translateService, primeng)
         }
 
-        return DoctorComponentHelper.instance
+        return PersonValidationHelper.instance
     }
 
     sendToastMessageSuccess(type: 'createDoctor' | 'updateDoctor' | 'deleteDoctor' | 'deleteDoctors', param: string) {
@@ -31,6 +32,81 @@ export class DoctorComponentHelper extends ValidatorHelper {
 
         if (!clinic) {
             return this.getText(this.validationsKey + 'selectionRequired')
+        }
+
+        return undefined
+    }
+
+    validateName(firstName: string): string | undefined {
+
+        const message = this.validateField(firstName)
+        if (message) {
+            return message
+        }
+
+        const hasNumber = /\d/.test(firstName);
+        if (hasNumber) {
+            return this.getText(this.validationsKey + 'noNumbers');
+        }
+
+        const invalidChar = /[^A-Za-zÀ-ÿ ]/.test(firstName);
+        if (invalidChar) {
+            return this.getText(this.validationsKey + 'noSpecialChars');
+        }
+
+        return undefined
+    }
+
+    validateFieldNumber(postalCode: string, maxLength: number): string | undefined {
+        const message = this.validateField(postalCode, maxLength)
+        if (message) {
+            return message
+        }
+
+        const invalidChar = /[^A-Za-zÀ-ÿ ]/.test(postalCode);
+        if (!invalidChar) {
+            return this.getText(this.validationsKey + 'noSpecialChars');
+        }
+
+        if (!/^\d+$/.test(postalCode)) {
+            return this.getText(this.validationsKey + 'onlyNumbers');
+        }
+
+        return undefined
+    }
+
+    validateEmail(email: string) {
+        const message = this.validateField(email)
+        if (message) {
+            return message
+        }
+
+        if (!validator.isEmail(email)) {
+            return this.getText(this.validationsKey + 'email')
+        }
+
+        return undefined
+    }
+
+    validateBirthDate(date?: Date) {
+        if (!date) {
+            return this.getText(this.validationsKey + 'required')
+        }
+
+        const today = new Date()
+
+        let age = today.getFullYear() - date.getFullYear();
+        const monthDiff = today.getMonth() - date.getMonth();
+        const dayDiff = today.getDate() - date.getDate();
+
+        // Ajustar si aún no ha cumplido años este año
+        if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+            age--;
+        }
+
+        // 2. Verificar que tenga al menos 18 años
+        if (age < 18) {
+            return this.getText(this.validationsKey + 'birthDateAdult')
         }
 
         return undefined
