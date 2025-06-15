6d041737e768bc93517564469b3541e305bc0911
Daniel <danielmatt1701@gmail.com>
2025-06-15 00:31:49 -0600

    Añadir funciones para buscar colores
    - Se añadieron colores dependiendo de un valor, como las afecciones,
      resultados o el género
    - Se añadieron funciones para buscar los colores

--- src/app/core/theme/colors.ts
+++ src/app/core/theme/colors.ts
@@ -4,3 +4,21 @@ export const mapColors = {
     'blue': '#3b82f6',
     'green': '#22c55e'
 }
+
+export const genderColor = {
+    'male': '#2196f3',
+    'female': '#E91E63'
+}
+
+export const resultColor = {
+    'proliferative': mapColors.red,
+    'moderate': mapColors.amber,
+    'mild': mapColors.blue,
+    'nocondition': mapColors.green
+}
+
+export const diseaseColor = {
+    'dryAmd': '#fbc02d',
+    'wetAmd': '#009688',
+    'diabeticRetinopathy': '#9c27b0'
+}
