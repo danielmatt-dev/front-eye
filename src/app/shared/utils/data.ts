0a8e1d789a85b6dc619794f769851751027e4e12
Daniel <danielmatt1701@gmail.com>
2025-06-15 00:28:00 -0600

    Añadir archivo con datos
    - Se definió un archivo con un catálogo de datos, disease, results y
      genders

---
+++ src/app/shared/utils/data.ts
@@ -0,0 +1,18 @@
+export const diseases = [
+    'DMAE Seca',
+    'DMAE Húmeda',
+    'Retinopatía Diabética'
+]
+
+export const ageRanges = [
+    'Menos de 30',
+    'De 30 a 45',
+    'Más de 45'
+]
+
+export const results = [
+    'Proliferativo',
+    'Moderado',
+    'Leve',
+    'Sin Afección'
+]
