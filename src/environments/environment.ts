392bd2bea4eb6149417d7ed0e30ca2f6d133c138
Daniel <danielmatt1701@gmail.com>
2025-06-20 19:37:45 -0600

    Añadir variable recoveryToken

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:8080/insights'
+    apiBaseUrl: 'http://localhost:8080/insights',
+    recoveryToken: '1234'
 };
