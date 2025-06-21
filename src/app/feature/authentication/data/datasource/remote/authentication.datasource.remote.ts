3143a6ab8b6e6eb354db37f215cdbea5ce230507
Daniel <danielmatt1701@gmail.com>
2025-06-20 19:36:07 -0600

    Actualizar parámetros en AuthenticationDatasource

--- src/app/feature/authentication/data/datasource/remote/authentication.datasource.remote.ts
+++ src/app/feature/authentication/data/datasource/remote/authentication.datasource.remote.ts
@@ -1,15 +1,15 @@
 import { UserModel } from '../../models/user.model';
 import { Either } from 'fp-ts/Either';
 import { AuthResponseModel } from '../../models/auth.response.model';
-import { RecoveryTokenModel } from '../../models/recovery.token.model';
+import { ResetTokenModel } from '../../models/resetTokenModel';
 
 export interface AuthenticationDatasourceRemote {
 
     // <>
     login(user: UserModel): Promise<Either<Error, AuthResponseModel>>
 
-    validateEmail(email: string): Promise<Either<Error, RecoveryTokenModel>>
+    validateEmail(email: string, recoveryToken: string): Promise<Either<Error, ResetTokenModel>>
 
-    resetPassword(user: UserModel): Promise<Either<Error, boolean>>
+    resetPassword(user: UserModel, resetToken: string): Promise<Either<Error, boolean>>
 
 }
