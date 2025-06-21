ce7698c575a8584d7807841a364734b54741e224
Daniel <danielmatt1701@gmail.com>
2025-06-20 19:05:16 -0600

    Actualizar AuthenticationDatasourceRemote
    - Se implementó la función para validar el correo del usuario
    - Se implemetó una función para reestablecer la contraseña del usuario
      enviando el correo y contraseña

--- src/app/feature/authentication/data/datasource/remote/authentication.datasource.remote.ts
+++ src/app/feature/authentication/data/datasource/remote/authentication.datasource.remote.ts
@@ -1,10 +1,15 @@
 import { UserModel } from '../../models/user.model';
 import { Either } from 'fp-ts/Either';
 import { AuthResponseModel } from '../../models/auth.response.model';
+import { RecoveryTokenModel } from '../../models/recovery.token.model';
 
 export interface AuthenticationDatasourceRemote {
 
     // <>
     login(user: UserModel): Promise<Either<Error, AuthResponseModel>>
+
+    validateEmail(email: string): Promise<Either<Error, RecoveryTokenModel>>
+
+    resetPassword(user: UserModel): Promise<Either<Error, boolean>>
 
 }
--- src/app/feature/authentication/data/datasource/remote/impl/authentication.datasource.remote.impl.ts
+++ src/app/feature/authentication/data/datasource/remote/impl/authentication.datasource.remote.impl.ts
@@ -3,15 +3,15 @@ import { AuthResponseModel } from '../../../models/auth.response.model';
 import { UserModel } from '../../../models/user.model';
 import { AuthenticationDatasourceRemote } from '../authentication.datasource.remote';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ApiService } from '../../../../../../shared/services/api.service';
 import { AuthEndpoints } from '../auth.endpoints';
 import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { map } from 'rxjs/operators';
+import { RecoveryTokenModel } from '../../../models/recovery.token.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationDatasourceRemoteImpl implements AuthenticationDatasourceRemote {
-
     constructor(
         private readonly http: HttpClient,
         private readonly apiService: ApiService
@@ -24,6 +24,30 @@ export class AuthenticationDatasourceRemoteImpl implements AuthenticationDatasou
             .pipe(
                 map(response =>
                     plainToInstance(AuthResponseModel, response)))
+
+        return this.apiService.sendRequest(obs$)
+    }
+
+    validateEmail(email: string): Promise<Either<Error, RecoveryTokenModel>> {
+        const url = AuthEndpoints.PATH_RESET;
+
+        const params = new HttpParams()
+            .set('email', email)
+
+        const obs$ = this.http
+            .get<RecoveryTokenModel>(url, { params })
+            .pipe(
+                map(response => plainToInstance(RecoveryTokenModel, response)))
+
+        return this.apiService.sendRequest(obs$)
+    }
+
+    resetPassword(user: UserModel): Promise<Either<Error, boolean>> {
+        const url = AuthEndpoints.PATH_RESET;
+        const obs$ = this.http
+            .post<boolean>(url, instanceToPlain(user))
+            .pipe(
+                map(() => true))
 
         return this.apiService.sendRequest(obs$)
     }
