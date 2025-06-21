cc6c0acb9cc9c04ac368151c2aad5047e1cd7b65
Daniel <danielmatt1701@gmail.com>
2025-06-20 19:02:25 -0600

    Actualizar endpoints de auth

--- src/app/feature/authentication/data/datasource/remote/auth.endpoints.ts
+++ src/app/feature/authentication/data/datasource/remote/auth.endpoints.ts
@@ -2,4 +2,5 @@ import { BASE_URL } from '../../../../../shared/utils/base.url';
 
 export class AuthEndpoints {
     static readonly PATH_LOGIN = `${BASE_URL}/auth/login`;
+    static readonly PATH_RESET = `${BASE_URL}/auth/reset`;
 }
