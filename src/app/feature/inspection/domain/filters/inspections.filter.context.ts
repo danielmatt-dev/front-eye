fa87756e554aa1c8f9083ced425f8eaea33be96f
Daniel <danielmatt1701@gmail.com>
2025-08-31 20:45:57 -0600

    Añadir al contenedor de dependencias

--- src/app/feature/inspection/domain/filters/inspections.filter.context.ts
+++ src/app/feature/inspection/domain/filters/inspections.filter.context.ts
@@ -1,13 +1,19 @@
 import { InspectionsFilterStrategy } from './inspections.filter';
 import { ChartData } from 'chart.js';
 import { InspectionResponseModel } from '../../data/models/inspection.response.model';
+import { Injectable } from '@angular/core';
+import { TranslateLang } from '../../../../shared/utils/functions/translate-lang';
 
+@Injectable({ providedIn: 'root' })
 export class InspectionsFilterContext {
 
-    constructor(private readonly strategy: InspectionsFilterStrategy) {}
+    constructor(
+        private readonly translateLang: TranslateLang
+    ) {}
 
-    apply(data: InspectionResponseModel[]): ChartData {
-        return this.strategy.getChartData(data);
+    apply(data: InspectionResponseModel[], strategy: InspectionsFilterStrategy): ChartData {
+        strategy.setTranslateLang(this.translateLang)
+        return strategy.getChartData(data);
     }
 
 }
