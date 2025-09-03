1fe00c6cab9692b037ccc8b90773d40657141131
Daniel <danielmatt1701@gmail.com>
2025-09-03 08:30:51 -0600

    Actualizar comentarios en aimodel

--- src/app/feature/aimodel/data/model/aimodel.model.ts
+++ src/app/feature/aimodel/data/model/aimodel.model.ts
@@ -1,16 +1,49 @@
 import { Expose } from 'class-transformer';
 
+/**
+ * Modelo que representa un registro de un modelo de IA.
+ *
+ * @description
+ * Contiene la información básica de un modelo de inteligencia artificial,
+ * incluyendo su identificador, nombre, versión y descripción.
+ */
 export class AiModelModel {
 
+    /**
+     * Identificador único del modelo de IA.
+     *
+     * @example 101
+     */
     @Expose({ name: 'ai_model_id' })
     aiModelId?: number = undefined
 
+    /**
+     * Nombre del modelo de IA.
+     *
+     * @example "RetinaNet"
+     */
     name?: string = ''
 
+    /**
+     * Versión del modelo de IA.
+     *
+     * @example "v1.2.0"
+     */
     version?: string = ''
 
+    /**
+     * Descripción breve del modelo de IA.
+     *
+     * @example "Modelo entrenado para detección de enfermedades oculares."
+     */
     description?: string = ''
 
+    /**
+     * Constructor que permite inicializar el modelo
+     * con un objeto parcial de sus propiedades.
+     *
+     * @param partial Objeto con propiedades opcionales para inicializar la instancia.
+     */
     constructor(partial?: Partial<AiModelModel>) {
         Object.assign(this, partial)
     }
