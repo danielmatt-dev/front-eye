3902193643a5c1767162e1d9306eab68311ad9d1
Daniel <danielmatt1701@gmail.com>
2025-09-17 16:18:41 -0600

    Añadir botón de refresh en doctores

--- src/app/feature/doctor/presentation/doctor-component/doctor.component.ts
+++ src/app/feature/doctor/presentation/doctor-component/doctor.component.ts
@@ -147,8 +147,7 @@ export class DoctorComponent implements OnInit {
 
         reloadOnLangChange(this.translateService, this.destroyRef, this.loadGenders);
 
-        await this.callGetAllDoctors();
-        await this.callGetAllClinics();
+        await this.loadDoctors()
     }
 
     /* Traducciones de idioma */
@@ -166,6 +165,11 @@ export class DoctorComponent implements OnInit {
             doctor.gender = genderOption.value;
             return doctor;
         });
+    }
+
+    async loadDoctors() {
+        await this.callGetAllDoctors();
+        await this.callGetAllClinics();
     }
 
     /* Llamadas a casos de uso */
