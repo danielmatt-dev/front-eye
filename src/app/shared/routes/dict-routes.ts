be7095db2a79b7716b679386bd1cdeede5c26ae9
Daniel <danielmatt1701@gmail.com>
2025-09-24 22:37:52 -0600

    Añadir valores de rutas

---
+++ src/app/shared/routes/dict-routes.ts
@@ -0,0 +1,16 @@
+export const routes = {
+    dashboard: 'dashboard',
+    geographicData: 'geographic-data',
+    newInspection: 'new-inspection',
+    viewDetail: 'view-detail',
+    allInspections: 'all-inspections',
+    patients: 'patients',
+    doctors: 'doctors',
+    notfound: 'notfound'
+}
+
+export const authPathRoutes = {
+    login: 'login',
+    access: 'access',
+    reset: 'reset'
+}
